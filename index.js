7dec79ff37324715aec792ae2c6aed21c33a223e
bellashrns <bella.saharani@student.umn.ac.id>
2023-01-16 21:24:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json()); // menerima data dengan format json
 app.use(productRoute);
 
-app.listen(process.env.APP_PORT, () => {
-    console.log(`Server is running on port ${process.env.APP_PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
 
-export default app;
+module.exports = app;
