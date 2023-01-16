ab8e7b0ba306ad4d0512f8acd32462da46c5e70a
bellashrns <bella.saharani@student.umn.ac.id>
2023-01-16 21:09:59 +0700

    urus db

---
+++ config/db.config.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+const {
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_URL
+} = process.env;
+
+const db = new Sequelize(DB_URL, {
+    define: {
+        timestamps: false
+    }
+});
+
+module.exports = db;
