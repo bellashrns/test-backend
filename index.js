fb5cb429802c596a382deae535cdff6c464e211e
bellashrns <bella.saharani@student.umn.ac.id>
2023-01-16 20:32:55 +0700

    update database

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import productRoute from "./routes/ProductRoute.js";    
-import e from "express";
+import productRoute from "./routes/ProductRoute.js";
 
 dotenv.config();
+
+// const mysql = require('mysql');
+
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Database connected");
+//     }
+// });
 
 const app = express();
 
